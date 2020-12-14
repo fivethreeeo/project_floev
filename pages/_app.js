12eb50ab0813c9f8eeb62b023a9ec31dfe562993
san <gabriel@Mars.local>
2020-12-14 10:03:18 +0900

    ant design module added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import { ApolloProvider } from '@apollo/client'
 import { useApollo } from '../lib/apolloClient'
 
+import 'antd/dist/antd.css'
 import '../static/scss/main.scss'
+import '../static/customizedAntd/global.css'
 
 function FloevFrontApp({ Component, pageProps }) {
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 import Layout from '../layout/DefaultLayout'
 import HelloWorld from '../components/Helloworld'
 
+import { Button } from 'antd'
+
 const IndexPage = () => (
   <Layout title="Floev New Front">
     <div className="app">
       <HelloWorld />
     </div>
+    <Button type="primary" block>
+      Primary
+    </Button>
   </Layout>
 )
 
