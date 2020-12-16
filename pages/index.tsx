fead17651cc0de30eea4105ea81a9635fa139e09
sangbum.heo <sangbum.heo@floev.com>
2020-12-16 12:48:46 +0900

    index 마크업 작성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,122 @@
 import Layout from '../layout/DefaultLayout'
-import HelloWorld from '../components/Helloworld'
-
-import { Button } from 'antd'
 
 const IndexPage = () => (
-  <Layout title="Floev New Front">
-    <div className="app">
-      <HelloWorld />
+  <Layout title="플로브 - 나의 눈을 위한 안경 큐레이션 서비스">
+    <div className="indexWrap">
+    <div className="topBanner">
+                <a href="/2020-winter-event">
+                  <p className="gtm-003">[안경 기부 이벤트] 최대 8만원 할인 혜택<strong><span style={{ margin: '0 0 0 3px' }}> &#xE001;</span></strong></p>
+                </a>
+              </div>
+
+              <div className="section top">
+                <div className="inner">
+                  <div style={{ padding: '0 24px' }}>
+                    <p style={{ padding: '144px 0 0 0', color: '#fff', fontSize: '30px'}}>나의 눈을 위한<br />안경 큐레이션 서비스</p>
+                    <a className="gtm-001" style={{ display: 'inline-block', marginTop: '16px', background: '#c3512a', borderRadius: '32px', fontSize: '16px', color: '#fff', padding: '12px 20px' }}>플로브 시작하기</a>
+                  </div>
+                </div>
+                <div className="scroll-arrow"><div className="img"><img src="/static/img/newLanding/vv_w.png" alt="" /></div></div>
+              </div>
+
+              <div className="section pod">
+                <div className="inner" style={{ padding: '0 24px' }}>
+                  <p className="fontR" style={{ lineHeight: '1.13', fontSize: '32px', wordBreak: 'keep-all', color: '#64433f' }}>we care. we listen. we recommend.</p>
+                  <p style={{ marginTop: '16px', fontSize: '16px', color: '#64433f' }}>수많은 안경 속, 나에게 맞는 안경은 어떤 것일까 고민한 적 있으신가요?<br />눈 건강과 라이프스타일을 반영한 플로브 큐레이션을 경험하세요.</p>
+                </div>
+              </div>
+
+              <div className="section story">
+                <div className="sectionName">
+                  <p className="fontR">find story</p>
+                  <div className="underline"></div>
+                </div>
+                <div className="inner">
+                </div>
+              </div>
+
+              <div className="section service">
+                <div className="sectionName">
+                  <p className="fontR">service guide</p>
+                  <div className="underline"></div>
+                </div>
+                <div className="inner">
+                  <div className="descWrap">
+                    <div className="imgWrap">
+                      <div className="img"><img src="/static/img/newlanding/service_1.jpg" alt="" /></div>
+                      <div className="img"><img src="/static/img/newlanding/service_2.jpg" alt="" /></div>
+                      <div className="clearfix"></div>
+                    </div>
+                    <div className="txtWrap">
+                      <p className="fontR">01</p>
+                      <p>설문을 통해 준비된 추천 안경테</p>
+                      <p>설문을 통해 라이프 스타일, 시력 문제, 평소의 고민을 알려주세요.<br />플로브와 약속한 시간에 라운지에 방문하시면 설문을 바탕으로 추천된 16개의 브랜드 안경테를 받아볼 수 있습니다.</p>
+                    </div>
+                  </div>
+                  <div className="descWrap">
+                    <div className="imgWrap">
+                      <div className="img"><img src="/static/img/newlanding/service_3.jpg" alt="" /></div>
+                      <div className="img"><img src="/static/img/newlanding/service_4.jpg" alt="" /></div>
+                      <div className="clearfix"></div>
+                    </div>
+                    <div className="txtWrap">
+                      <p className="fontR">02</p>
+                      <p>전문적인 검안과 알맞는 렌즈 알아보기</p>
+                      <p>검안에 최적화된 공간에서 1대1로 꼼꼼히 검사를 받아요.<br />설문 내용과 검안 결과를 참고하여 내 눈에 필요한 렌즈가 어떤 것인지 설명들을 수 있습니다.</p>
+                    </div>
+                  </div>
+                  <div className="descWrap">
+                    <div className="imgWrap">
+                      <div className="img"><img src="/static/img/newlanding/service_5.jpg" alt="" /></div>
+                      <div className="img"><img src="/static/img/newlanding/service_6.jpg" alt="" /></div>
+                      <div className="clearfix"></div>
+                    </div>
+                    <div className="txtWrap">
+                      <p className="fontR">03</p>
+                      <p>자유로운 안경 체험과 상담</p>
+                      <p>추천 받은 다양한 브랜드 안경을 자유롭게 체험해요.<br />브랜드에 대한 스토리도 함께 알아가면서 편안한 상담을 할 수 있습니다.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="section message">
+                <div className="inner">
+                  <p className="fontR">" find your floev "</p>
+                  <p className="desc">나에게 딱 알맞는 안경을 찾아보세요!<br />오늘이 아니어도 괜찮습니다.<br />내 눈은 충분히 고려 되어야 하니까요.</p>
+                </div>
+              </div>
+
+              <div className="section lounge">
+                <div className="sectionName">
+                  <p className="fontR">lounge</p>
+                  <div className="underline"></div>
+                </div>
+              </div>
+
+              <div className="section bottom">
+                <div className="inner">
+                  <div className="cta-bottom"><a className="gtm-002" href="/survey">플로브 시작하기</a></div>
+                </div>
+              </div>
+
+              <div className="section company">
+                <p style={{ fontSize: '16px', color: '#33373a' }}>(주)씨에이치스퀘어 사업자 정보</p>
+                <ul style={{ marginTop: '20px', paddingBottom: '16px' }}>
+                  <li><span>대표자명</span><span>천영환</span></li>
+                  <li><span>사업자등록번호</span><span>699-86-01370</span></li>
+                  <li><span>통신판매업신고</span><span>2019-서울강남-02689</span></li>
+                  <li><span>주소</span><span>서울시 강남구 테헤란로4길 38-5, 6층</span></li>
+                  <li><span>전화/이메일</span><span><span style={{ paddingLeft: '0' }} >1544-8767</span> | contact@floev.com</span></li>
+                  <li><span>개인정보책임자</span><span>엄태산</span></li>
+                </ul>
+                <div style={{ marginTop: '12px', marginBottom: '8px ' }}>
+                  <a href="http://www.ftc.go.kr/bizCommPop.do?wrkr_no=6998601370" target="_blank"><span>사업자정보확인</span></a>
+                    |
+                    <a href="/service-policy"><span>서비스 정책 안내</span></a>
+                </div>
+              </div>
     </div>
-    <Button type="primary" block>
-      Primary
-    </Button>
   </Layout>
 )
 
