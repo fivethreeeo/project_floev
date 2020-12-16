a54f5e8a97f040a0b78440aa34628488f71b4d9f
sangbum.heo <sangbum.heo@floev.com>
2020-12-16 13:19:16 +0900

    index 마크업 작성

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,82 @@ import Layout from '../layout/DefaultLayout'
 const IndexPage = () => (
   <Layout title="플로브 - 나의 눈을 위한 안경 큐레이션 서비스">
     <div className="indexWrap">
+
+      <div className="top">
+        <div className="banner">
+          <a href="/2020-winter-event">[안경 기부 이벤트] 최대 8만원 할인 혜택<strong><span> &#xE001;</span></strong></a>
+        </div>
+        <div className="con">
+          <div className="inner">
+            <p className="txt">나의 눈을 위한<br/>안경 큐레이션 서비스</p>
+            <button className="cta">플로브 시작하기</button>
+          </div>
+          <div className="scroll-arrow"><div className="img"><img src="/static/img/newLanding/vv_w.png" alt="" /></div></div>
+        </div>
+      </div>
+
+      <div className="middle">
+        
+        <div className="sec pod">
+          <p>we care. we listen. we recommend.</p>
+          <p>수많은 안경 속, 나에게 맞는 안경은 어떤 것일까 고민한 적 있으신가요?<br />눈 건강과 라이프스타일을 반영한 플로브 큐레이션을 경험하세요.</p>
+        </div>
+
+        <div className="sec story">
+          <div className="sectionName">
+            <p className="fontR">find story</p>
+            <div className="underline"></div>
+          </div>
+          <div className="inner">
+          </div>
+        </div>
+
+        <div className="sec service">
+          <div className="sectionName">
+            <p className="fontR">service</p>
+            <div className="underline"></div>
+          </div>
+          <div className="inner">
+          </div>
+        </div>
+
+        <div className="sec message">
+
+        </div>
+
+        <div className="sec lounge">
+          <div className="sectionName">
+            <p className="fontR">lounge</p>
+            <div className="underline"></div>
+          </div>
+          <div className="inner">
+          </div>
+        </div>
+        
+      </div>
+
+      <div className="bottom">
+        <div className="con">
+          <div className="inner">
+            <button className="cta">플로브 시작하기</button>
+          </div>
+        </div>
+        <div className="footer">
+          <p>(주)씨에이치스퀘어 사업자 정보</p>
+          <ul>
+            <li><span>대표자명</span><span>천영환</span></li>
+            <li><span>사업자등록번호</span><span>699-86-01370</span></li>
+            <li><span>통신판매업신고</span><span>2019-서울강남-02689</span></li>
+            <li><span>주소</span><span>서울시 강남구 테헤란로4길 38-5, 6층</span></li>
+            <li><span>전화/이메일</span><span><span>1544-8767</span> | contact@floev.com</span></li>
+            <li><span>개인정보책임자</span><span>엄태산</span></li>
+          </ul>
+          <div>
+            <a href="http://www.ftc.go.kr/bizCommPop.do?wrkr_no=6998601370" target="_blank">사업자정보확인</a>
+          </div>
+        </div>
+      </div>
+
     <div className="topBanner">
                 <a href="/2020-winter-event">
                   <p className="gtm-003">[안경 기부 이벤트] 최대 8만원 할인 혜택<strong><span style={{ margin: '0 0 0 3px' }}> &#xE001;</span></strong></p>
