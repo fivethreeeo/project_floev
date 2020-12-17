3c3978dafbc0d106f58ffeabd715772e93929f54
san <san@sanscreate.com>
2020-12-17 21:40:53 +0900

    poopup added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,56 @@
 import Layout from '../layout/DefaultLayout'
+import React from 'react'
 import HelloWorld from '../components/Helloworld'
-import { Button } from 'antd-mobile'
+import { Modal, Button, List, WhiteSpace } from 'antd-mobile'
+//import {  } from 'antd'
 
-const IndexPage = () => (
-  <Layout title="Floev New Front">
-    <div className="app">
-      <HelloWorld />
-    </div>
-    <Button>
-      Primary
-    </Button>
-    
-  </Layout>
-)
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal1: false,
+      modal2: false,
+    };
+  }
+  showModal = key => (e) => {
+    e.preventDefault(); // 修复 Android 上点击穿透
+    this.setState({
+      [key]: true,
+    });
+  }
+  onClose = key => () => {
+    this.setState({
+      [key]: false,
+    });
+  }
+
+  render(){
+    return(
+      <Layout title="Floev New Front">
+        <div className="app">
+          <HelloWorld />
+        </div>
+        <Button onClick={this.showModal('modal2')}>팝업</Button>
+        <WhiteSpace />
+
+        <Button>
+          Primary
+        </Button>
+        <Modal
+          popup
+          visible={this.state.modal2}
+          onClose={this.onClose('modal2')}
+          animationType="slide-up"
+          afterClose={() => { alert('afterClose'); }}
+        >
+          <List renderHeader={() => <div>리스트</div>} className="popup-list">
+            <Button type="primary" onClick={this.onClose('modal2')}>닫기</Button>
+          </List>
+        </Modal>
+      
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage
