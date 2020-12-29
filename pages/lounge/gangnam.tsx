b198960ef2b19a9d1506a7475e9a7d5dc921c015
yolkinthehouse <sbsbheo@gmail.com>
2020-12-29 18:05:03 +0900

    라운지 개별 페이지 추가

---
+++ pages/lounge/gangnam.tsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import Layout from '../../layout/DefaultLayout'
+import Router from 'next/router'
+
+const gangnamPage = () => {
+
+    return (
+        <Layout title="플로브 - 나의 눈을 위한 안경 큐레이션 서비스">
+        <div className="global640"><div className="goBackBtnWrap"><div className="goBackBtn" onClick={() => Router.back()}></div></div></div>
+        <div className="wrap">
+        <div className="loungeWrap global640">
+
+<div className="title">
+  <div className="imgWrap"></div>
+  <div className="bk"></div>
+  <div className="txtWrap">
+    <p>라운지</p>
+    <p>강남</p>
+    <p>복잡한 강남 가운데서 담백하게 쉼을 제안하는 곳,<br/>플로브 라운지 강남입니다.</p>
+    <div className="map"><p><a className="gtm-010" target="_blank" href="https://m.map.naver.com/search2/site.nhn?query=%ED%94%8C%EB%A1%9C%EB%B8%8C&sm=hty&style=v5&code=1814217589">라운지 위치보기 &#xE001;</a></p></div>
+  </div>
+</div>
+
+<div style={{marginTop:'24px'}} className="section">
+  <p>플로브는 자신의 취향을 발견하는 것도 서비스의 중요한 가치라고 생각해요. 오늘 당장 마음에 드는 안경을 발견하지 못해도 괜찮아요.<br/><br/><strong>나의 취향과 라이프스타일에 맞게 안경을 선택해낼 수 있다는 것</strong> 그 자체로도 가치가 있죠.</p>
+</div>
+
+<div className="section">
+  <div className="imgWrap"><img src="/static/img/newLanding/lounge-g-2.jpg" alt=""/></div>
+  <p><strong>'어떻게 하면 더 많은 사람이 플로브를 통해 가치있는 발견을 할 수 있을까?',<br/>'어떻게 하면 필요한 만큼 편리하게 플로브 서비스를 더 이용할 수 있을까?'</strong><br/><br/>이 두 가지를 고민했고,<br/>다양한 시도 끝에 보다 확장된 플로브를 경험할 수 있는 라운지가 탄생했어요.</p>
+</div>
+
+<div className="section">
+  <div className="imgWrap"><img src="/static/img/newLanding/lounge-g-5.jpg" alt=""/></div>
+  <p>확장된 라운지에서는 충분히 독립된 상담 공간에서 이전처럼 플로브 서비스를 집중해서 온전히 받을 수 있고 <strong>상담 데스크 이외에 커뮤니티 데스크와 피팅 데스크, 대기공간이 있어 큐레이션 이외의 플로브 케어를 더욱 편리하게 이용할 수 있어요.</strong></p>
+</div>
+
+<div className="section">
+  <div className="imgWrap"><img src="/static/img/newLanding/lounge-g-6.jpg" alt=""/></div>
+  <p>한층 더 밝고 세련된 무드를 가진 라운지 강남에서도 역삼과 동일한 플로브 시그니처 상담 데스크를 발견할 수 있고, 동일한 플로브 뮤직 블랜드를 경험할 수 있답니다.</p><br/>
+  <p>우리는 어려운 서비스를 지향하지 않아요. 당신에게 보다 세심하게 다가가고 싶어요. 플로브가 지향하는 ‘발견하는 즐거움'을 새로운 공간에서 경험하세요.</p>
+</div>
+
+<div className="section reco">
+  <p>플로브 라운지 강남, 이런 분에게 추천해요!</p>
+  <p>좀 더 나에게 필요한 시간에 라운지를 이용하고 싶어요.</p>
+  <p>다양하게 주어진 시간대를 나의 일정에 맞추어 활용하고 싶어요.</p>
+  <p>피팅이나 수리 등 다른 케어 서비스를 자주 편하게 예약하고 싶어요.</p>
+  <p>부담없이 자유로운 분위기에서 서비스 받고 싶어요.</p>
+</div>
+
+</div>
+          <style jsx>{`
+          strong {color:#64433F}
+          .newColor {color:#C3512A}
+
+          .loungeWrap {padding-bottom:80px}
+          .section .imgWrap {float:none;width:100%;margin:0 0 16px 0;}
+          .section .imgWrap img {width:100%}
+
+          .section {margin:0 0 40px}
+          .section:nth-child(2) {margin:0 0 40px}
+          .section p {padding:0 24px;font-size:16px;line-height:30px}
+          .section img {width:100%}
+
+          .title {position:relative}
+          .title .imgWrap {float:none;width:100%;height:640px;overflow:hidden;background:url(/static/img/newLanding/lounge-g-1.jpg) center center / cover no-repeat}
+          .title .bk {position:absolute;top:0;right:0;bottom:0;left:0;background:rgba(0,0,0,0.3)}
+          .title .txtWrap {position:absolute;top:100px;padding:0 24px}
+          .title .txtWrap p {color:#fff;font-size:32px;line-height:46px;font-weight:bold}
+          .title .txtWrap p:nth-child(3) {margin:24px 0 0 4px;font-size:14px;line-height:24px;font-weight:400}
+          .title .map {margin-top:24px;width:160px;height:40px;border-radius:4px;overflow:hidden;text-align:center}
+          .title .map p {line-height:40px;font-size:14px;font-weight:400;color:#fff;background:#C3512a}
+          .title .map p a {display:block;color:#fff}
+          
+          .end {padding-bottom:40px}
+          .reco {display:none;background:#64433f}
+          .reco p {color:#fff}
+          @media (min-width: 640px) {
+            .imgWrap img {border-radius:2px}
+            .title {margin:0 auto}
+            .title .txtWrap {top:200px;padding:0 48px}
+            .title .imgWrap {overflow:hidden;border-bottom-left-radius:2px;border-bottom-right-radius:2px}
+            .title .bk {overflow:hidden;border-bottom-left-radius:2px;border-bottom-right-radius:2px}
+            .section p {padding:0 16px}
+          }
+        `}</style>
+        </div>
+        </Layout>
+        
+    )
+}
+
+export default gangnamPage
---
+++ pages/lounge/yeoksam.tsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import Layout from '../../layout/DefaultLayout'
+import Router from 'next/router'
+
+const yeoksamPage = () => {
+
+    return (
+        <Layout title="플로브 - 나의 눈을 위한 안경 큐레이션 서비스">
+        <div className="global640"><div className="goBackBtnWrap"><div className="goBackBtn" onClick={() => Router.back()}></div></div></div>
+        <div className="wrap">
+        <div className="loungeWrap global640">
+
+<div className="title">
+  <div className="imgWrap"></div>
+  <div className="bk"></div>
+  <div className="txtWrap">
+    <p>라운지</p>
+    <p>역삼성당</p>
+    <p>큰 창 너머 붉은 벽돌의 역삼성당이 아름다운<br/>플로브 라운지 역삼성당 입니다.</p>
+    <div className="map"><p><a className="gtm-010" target="_blank" href="https://m.map.naver.com/search2/site.nhn?query=%EC%97%AD%EC%82%BC%20%ED%94%8C%EB%A1%9C%EB%B8%8C&sm=hty&style=v5&code=1175801694">라운지 위치보기 &#xE001;</a></p></div>
+  </div>
+</div>
+
+<div style={{marginTop:'24px'}} className="section">
+  <p>라운지 역삼성당은 플로브 서비스가 발을 디딘 첫 매장으로 <br/><strong>‘we listen we care we recommend’</strong><br/> 플로브가 고객에게 전달하고 싶은 가치를 온전하게 선보이는 것에 최적화된 공간이에요.</p><br/>
+  
+</div>
+
+<div className="section">
+  <div className="imgWrap"><img src="/static/img/newLanding/lounge-y-2.jpg" alt=""/></div>
+  <p>기존 안경원이 가진 특유의 딱딱한 판매 공간이 아닌 <strong>눈 맞추고 대화하는 것에 집중하여 나의 눈을 정확하게 알고 필요한 선택을 충분히 고민할 수 있는 공간</strong>을 만들고 싶었어요.</p>
+</div>
+
+<div className="section">
+  <div className="imgWrap"><img src="/static/img/newLanding/lounge-y-3.jpg" alt=""/></div>
+  <p>미리 예약한 시간에 라운지 역삼성당을 방문하면 플로브 서비스를 <strong>안경 카운셀러와 함께 1:1로 경험하며 라운지 전체를 온전하게 이용할 수 있는 점</strong>이 역삼성당 매장의 가장 큰 매력 포인트죠.</p>
+</div>
+
+<div className="section">
+  <div className="imgWrap"><img src="/static/img/newLanding/lounge-y-4.jpg" alt=""/></div>
+  <p>나만의 라운지에서 흘러나오는 <br/><strong>플로브의 뮤직 블랜드와 곳곳의 취향에 맞는 소품에서 소소한 재미</strong>를 발견하세요.</p>
+</div>
+
+<div className="section reco">
+  <p>플로브 라운지 역삼성당, 이런 분에게 추천해요!</p>
+  <p>50분 동안 오로지 나에게만 주어진 공간에서 독립된 서비스를 경험하고 싶어요.</p>
+  <p>부모님이나 사랑하는 사람에게 새로운 경험과 좋은 안경을 오붓하게 선물하고 싶어요.</p>
+  <p>조용하고 분위기 있는 공간에서 서비스 받고 싶어요.</p>
+</div>
+
+</div>
+
+          <style jsx>{`
+          strong {color:#64433F}
+          .newColor {color:#C3512A}
+
+          .loungeWrap {padding-bottom:80px}
+          .section .imgWrap {float:none;width:100%;margin:0 0 16px 0;}
+          .section .imgWrap img {width:100%}
+
+          .section {margin:0 0 40px}
+          .section:nth-child(2) {margin:0 0 40px}
+          .section p {padding:0 24px;font-size:16px;line-height:30px}
+          .section img {width:100%}
+
+          .title {position:relative}
+          .title .imgWrap {float:none;width:100%;height:640px;overflow:hidden;background:url(/static/img/newLanding/lounge-y-1.jpg) center center / cover no-repeat}
+          .title .bk {position:absolute;top:0;right:0;bottom:0;left:0;background:rgba(0,0,0,0.5)}
+          .title .txtWrap {position:absolute;top:100px;padding:0 24px}
+          .title .txtWrap p {color:#fff;font-size:32px;line-height:46px;font-weight:bold}
+          .title .txtWrap p:nth-child(3) {margin:24px 0 0 4px;font-size:14px;line-height:24px;font-weight:400}
+          .title .map {margin-top:24px;width:160px;height:40px;border-radius:4px;overflow:hidden;text-align:center}
+          .title .map p {line-height:40px;font-size:14px;font-weight:400;color:#fff;background:#C3512a}
+          .title .map p a {display:block;color:#fff}
+          
+          .end {padding-bottom:40px}
+          .reco {display:none;background:#64433f}
+          .reco p {color:#fff}
+          @media (min-width: 640px) {
+            .imgWrap img {border-radius:2px}
+            .title {margin:0 auto}
+            .title .txtWrap {top:200px;padding:0 48px}
+            .title .imgWrap {overflow:hidden;border-bottom-left-radius:2px;border-bottom-right-radius:2px}
+            .title .bk {overflow:hidden;border-bottom-left-radius:2px;border-bottom-right-radius:2px}
+            .section p {padding:0 16px}
+          }
+        `}</style>
+        </div>
+        </Layout>
+        
+    )
+}
+
+export default yeoksamPage
