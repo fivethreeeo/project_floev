0c8e1ee9162a0e6711e6c5474cf4ed2072384c32
yolkinthehouse <sbsbheo@gmail.com>
2021-01-04 03:34:12 +0900

    nav, event-banner 레이아웃 조정

--- pages/index.tsx
+++ pages/index.tsx
@@ -166,13 +166,11 @@ const IndexPage = () => {
 				<div className="header__inner">
 					<div className="header__logo"><a href="/"><img src="/static/img/newLanding/fv_logo_bk.svg" alt="" /></a></div>
 					<nav className="header__nav">
-						<ul>
-							<li><a href="/lounge/yeoksam">라운지 역삼성당</a></li>
-							<li><a href="/lounge/gangnam">라운지 강남</a></li>
-							<li><a href="/brand">플로브 추천 안경</a></li>
-							<li><a href="/service-policy">서비스 정책 안내</a></li>
-							<li><a href="https://www.instagram.com/floev_official/" target="_blank"><img src="/static/img/newLanding/i_insta.svg" alt="" /></a></li>
-						</ul>
+							<span><a href="/lounge/yeoksam">라운지 역삼성당</a></span>
+							<span><a href="/lounge/gangnam">라운지 강남</a></span>
+							<span><a href="/brand">플로브 추천 안경</a></span>
+							<span><a href="/service-policy">서비스 정책 안내</a></span>
+							<span><a href="https://www.instagram.com/floev_official/" target="_blank"><img src="/static/img/newLanding/i_insta.svg" alt="" /></a></span>
 					</nav>
 				</div>
 			</header>
