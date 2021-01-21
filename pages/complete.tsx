00b394ef1d0d8b9d5619e8e0104a9dd70e54af76
InBaeJang <inbaejj117@gmail.com>
2021-01-21 10:02:43 +0900

    complete page dummy data injection

--- pages/complete.tsx
+++ pages/complete.tsx
@@ -3,12 +3,12 @@ import Head from "next/head";
 import { GetServerSideProps } from "next";
 import { createApolloClient } from '../lib/apolloClient'
 import { gql } from '@apollo/client'
-import { getMDW, getHour } from '../utils/timeFormat'
+// import { getMDW, getHour } from '../utils/timeFormat'
 
 const CompletePage = (props: {
     user: any
 }) => {
-    const userSchedule: Schedule = props.user.reservations[props.user.reservations.length - 1]
+    // const userSchedule: Schedule = props.user.reservations[props.user.reservations.length - 1]
 
 
     return (<>
@@ -20,14 +20,19 @@ const CompletePage = (props: {
           </script>
         </Head>
         <div className="contentWrap">
-            <p className="qDesc3">{props.user.name}님 <strong>서비스 예약 일정</strong>은<br />
+            <p className="qDesc3">jtest님 <strong>서비스 예약 일정</strong>은<br />
+                <strong>1월 19일 화요일 오전 11:01</strong><br />
+                <strong>예약 라운지</strong>는 역삼성당입니다.<br />
+            </p>
+            {/* <p className="qDesc3">{props.user.name}님 <strong>서비스 예약 일정</strong>은<br />
                 <strong>{getMDW(userSchedule.date)} {getHour(userSchedule.date)}</strong><br />
                 <strong>예약 라운지</strong>는 {userSchedule.loungeCode === 1 ? '역삼성당' : '강남'}입니다.<br />
-            </p>
+            </p> */}
             <p>- 쿠폰은 라운지에서 체험 후 구매를 원하시면 자동 적용됩니다.</p>
             <p>- 결제할 때 별도의 쿠폰 이미지를 제시하실 필요는 없습니다.</p>
             <p>- 다른 할인 이벤트와 중복으로 사용 가능합니다.</p>
             <p>- 예약 취소 시 쿠폰은 자동 소멸됩니다.</p>
+            {props.user === null ? '' : props.user.name}
         </div>
     </>)
 }
