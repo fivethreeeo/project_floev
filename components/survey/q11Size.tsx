3f96c0f4ebc6f71575ec51d3229f5867ecd3a811
InBaeJang <inbaejj117@gmail.com>
2021-01-25 13:53:14 +0900

    purposes, painTypes checkbox & size 3 input text

--- components/survey/q11Size.tsx
+++ components/survey/q11Size.tsx
@@ -9,17 +9,48 @@ export default function Q11Size(props: {
     onPrev: () => void
     onNext: () => void
 }) {
-    const [size, setSize] = useState<string>(props.oldAnswers.size)
+    const [size1, setSize1] = useState<string>('')
+    const [size2, setSize2] = useState<string>('')
+    const [size3, setSize3] = useState<string>('')
+    const [totalSize, setTotalSize] = useState<string>(props.oldAnswers.size)
 
-    function handleChange(newSize: string) {
-        setSize(newSize) //화면 표시를 위한 const control
+    function handleChange1(newSize: string) {
+        setSize1(newSize)
+        const tempTotalSize = newSize + '-' + size2 + '-' + size3
+        setTotalSize(tempTotalSize)
+
         let answersParam: Answers = props.oldAnswers
-        answersParam.size = newSize
+        answersParam.size = tempTotalSize
         props.answersUpdate(answersParam)
 
-        // for caching
         localStorage.setItem('floev[currentStep]', '11')
-        localStorage.setItem('floev[size]', newSize)
+        localStorage.setItem('floev[size]', tempTotalSize)
+    }
+
+    function handleChange2(newSize: string) {
+        setSize2(newSize)
+        const tempTotalSize = size1 + '-' + newSize + '-' + size3
+        setTotalSize(tempTotalSize)
+
+        let answersParam: Answers = props.oldAnswers
+        answersParam.size = tempTotalSize
+        props.answersUpdate(answersParam)
+
+        localStorage.setItem('floev[currentStep]', '11')
+        localStorage.setItem('floev[size]', tempTotalSize)
+    }
+
+    function handleChange3(newSize: string) {
+        setSize3(newSize)
+        const tempTotalSize = size1 + '-' + size2 + '-' + newSize
+        setTotalSize(tempTotalSize)
+
+        let answersParam: Answers = props.oldAnswers
+        answersParam.size = tempTotalSize
+        props.answersUpdate(answersParam)
+
+        localStorage.setItem('floev[currentStep]', '11')
+        localStorage.setItem('floev[size]', tempTotalSize)
     }
 
     return (<>
@@ -27,19 +58,16 @@ export default function Q11Size(props: {
             <p className="qDesc">사진을 참고하여 내 안경의 사이즈를 입력해주세요.</p>
             <p className="qDesc2">지워지거나 원래 적혀있지 않은 안경도 있어요. 이 경우 편하게 넘어가셔도 됩니다.</p>
             <div className="answerWrap">
-                <input type="text" name="size" placeholder={'45'} value={size} maxLength={9} onChange={e => handleChange(e.target.value)} />
-                <input type="text" name="size" placeholder={'24'} value={size} maxLength={9} onChange={e => handleChange(e.target.value)} />
-                <input type="text" name="size" placeholder={'145'} value={size} maxLength={9} onChange={e => handleChange(e.target.value)} />
+                <input type="text" name="size1" placeholder={'45'} value={size1} maxLength={2} onChange={e => handleChange1(e.target.value)} />
+                <input type="text" name="size2" placeholder={'24'} value={size2} maxLength={2} onChange={e => handleChange2(e.target.value)} />
+                <input type="text" name="size3" placeholder={'145'} value={size3} maxLength={3} onChange={e => handleChange3(e.target.value)} />
             </div>
         </div>
         <div className="btnWrap">
-            <button className="btnNext gtm-016" type="button" onClick={() => props.onNext()}>다음</button>
-            {/*
-                {size === '' ?
-                    (<button className="btnNext disabled" type="button">다음</button>) :
+            {totalSize.length !== 9 ?
+                (<button className="btnNext disabled" type="button">다음</button>) :
                 (<button className="btnNext gtm-016" type="button" onClick={() => props.onNext()}>다음</button>)}
-            */}
         </div>
-        <button className="btn btn01 gtm-012" style={{ fontSize: '16px', borderRadius: '24px'}} type="button" disabled={props.currentStep !== props.max ? false : true} onClick={() => props.onPrev()}>뒤로</button>
+        <button className="btn btn01 gtm-012" style={{ fontSize: '16px', borderRadius: '24px' }} type="button" disabled={props.currentStep !== props.max ? false : true} onClick={() => props.onPrev()}>뒤로</button>
     </>)
 }
--- components/survey/q6Purposes.tsx
+++ components/survey/q6Purposes.tsx
@@ -14,14 +14,13 @@ export default function Q6Purpose(props: {
 
     function handleChangePurpose(e: any) {
         const newPurpose = e.target.value
-        let newPurposes: string[] = purposes
+        let newPurposes: string[] = [...purposes]
 
         if (e.target.checked) {
             newPurposes.push(newPurpose)
         } else {
-            newPurposes = newPurposes.filter(n => n !== newPurpose)
+            newPurposes = newPurposes.filter(purpose => purpose !== newPurpose)
         }
-
         setPurposes(newPurposes)
 
         let answersParam: Answers = props.oldAnswers
@@ -45,29 +44,29 @@ export default function Q6Purpose(props: {
     }
 
     return (<>
-                <div className="q-wrap q6">
-                    <div className="q-wrap__question-main">어떤 용도의 안경을 추천해드릴까요?</div>
-                    <div className="q-wrap__answer-wrap q-wrap__checkbox-wrap" onChange={e => handleChangePurpose(e)}>
-                        <input className="q-wrap__input-checkbox" type="checkbox" id="q6_1" onChange={() => { }} value="daily" checked={purposes.includes("daily")} />
-                        <label className="q-wrap__label-checkbox" htmlFor="q6_1">일상/데일리용 안경</label>
-                        <input className="q-wrap__input-checkbox" type="checkbox" id="q6_2" onChange={() => { }} value="work" checked={purposes.includes("work")} />
-                        <label className="q-wrap__label-checkbox" htmlFor="q6_2">업무/컴퓨터 작업용 안경</label>
-                        <p className="q-wrap__textarea-caption">* 그 외 용도나 구체적인 상황을 더 들려주세요.</p>
-                        <textarea
-                            className="q-wrap__textarea"
-                            value={purposeEtc ?? ''}
-                            onChange={e => handleChangePuposeEtc(e)}
-                            placeholder="예시) 독서용 안경이 필요해요. &#13;&#10;집에서만 렌즈 대신 착용하는 안경이에요."
-                        ></textarea>
-                    </div>
-                    <div className="btnWrap">
-                        {(purposes[0] === '' && purposeEtc.length === 0) ? (
-                            <button className="btnNext disabled" type="button">다음</button>) :
-                            (<button className="btnNext gtm-016" type="button" onClick={() => props.onNext()}>다음</button>)
-                        }
-                    </div>
-                    <button className="btn btn01 gtm-012" style={{ fontSize: '16px', borderRadius: '24px'}} type="button" disabled={props.currentStep !== props.max ? false : true} onClick={() => props.onPrev()}>뒤로</button>
-                </div>
+        <div className="q-wrap q6">
+            <div className="q-wrap__question-main">어떤 용도의 안경을 추천해드릴까요?</div>
+            <div className="q-wrap__answer-wrap q-wrap__checkbox-wrap" onChange={e => handleChangePurpose(e)}>
+                <input className="q-wrap__input-checkbox" type="checkbox" id="q6_1" onChange={() => { }} value="daily" checked={purposes.includes("daily")} />
+                <label className="q-wrap__label-checkbox" htmlFor="q6_1">일상/데일리용 안경</label>
+                <input className="q-wrap__input-checkbox" type="checkbox" id="q6_2" onChange={() => { }} value="work" checked={purposes.includes("work")} />
+                <label className="q-wrap__label-checkbox" htmlFor="q6_2">업무/컴퓨터 작업용 안경</label>
+                <p className="q-wrap__textarea-caption">* 그 외 용도나 구체적인 상황을 더 들려주세요.</p>
+                <textarea
+                    className="q-wrap__textarea"
+                    value={purposeEtc ?? ''}
+                    onChange={e => handleChangePuposeEtc(e)}
+                    placeholder="예시) 독서용 안경이 필요해요. &#13;&#10;집에서만 렌즈 대신 착용하는 안경이에요."
+                ></textarea>
+            </div>
+            <div className="btnWrap">
+                {(purposes.length === 0 && purposeEtc.length === 0) ? (
+                    <button className="btnNext disabled" type="button">다음</button>) :
+                    (<button className="btnNext gtm-016" type="button" onClick={() => props.onNext()}>다음</button>)
+                }
+            </div>
+            <button className="btn btn01 gtm-012" style={{ fontSize: '16px', borderRadius: '24px' }} type="button" disabled={props.currentStep !== props.max ? false : true} onClick={() => props.onPrev()}>뒤로</button>
+        </div>
         <div className="contentWrap">
 
         </div>
