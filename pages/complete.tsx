4c5cc6d3933a0410251805a21be533bbc6336f51
InBaeJang <inbaejj117@gmail.com>
2021-01-26 09:38:33 +0900

    modify complete page

--- pages/complete.tsx
+++ pages/complete.tsx
@@ -4,12 +4,12 @@ import Head from "next/head";
 import { GetServerSideProps } from "next";
 import { createApolloClient } from '../lib/apolloClient'
 import { gql } from '@apollo/client'
-// import { getMDW, getHour } from '../utils/timeFormat'
+import { getMDW, getHour } from '../utils/timeFormat'
 
 const CompletePage = (props: {
     user: any
 }) => {
-    // const userSchedule: Schedule = props.user.requests[props.user.requests.length - 1]
+    const userSchedule: Schedule = props.user.requests[props.user.requests.length - 1]
 
     return (<>
         <Head>
@@ -20,43 +20,29 @@ const CompletePage = (props: {
           </script>
         </Head>
         <Layout>
-                <div className="complete">
-                    <div className="request">
-                        <div className="request__msg">설문이 완료되었어요.</div>
-                        <div className="request__info">
-                            <p className="request__info-inner">
-                                <p>예약 정보</p>
-                                {props.user === null ? '' : props.user.name}허상범님 <strong>서비스 예약 일정</strong>은<br />
-                                <strong className="schedule">1월 19일 화요일 오전 11:00</strong><br />
-                                <strong>예약 라운지는 </strong><strong className="lounge">역삼성당</strong> 입니다.
-                            </p>
-                            {/* <p className="qDesc3">{props.user.name}님 <strong>서비스 예약 일정</strong>은<br />
-                                <strong>{getMDW(userSchedule.date)} {getHour(userSchedule.date)}</strong><br />
-                                <strong>예약 라운지</strong>는 {userSchedule.loungeCode === 1 ? '역삼성당' : '강남'}입니다.<br />
-                            </p> */}
-
-                        </div>
-
+            <div className="complete">
+                <div className="request">
+                    <div className="request__msg">설문이 완료되었어요.</div>
+                    <div className="request__info">
+                        <div className="request__info-inner">
+                            <p>예약 정보</p>
+                            {props.user === null ? '' : props.user.name}님 <strong>서비스 예약 일정</strong>은<br />
+                            <strong className="schedule">{getMDW(userSchedule.date)} {getHour(userSchedule.date)}</strong><br />
+                            <strong>예약 라운지는 </strong><strong className="lounge">{userSchedule.loungeCode === 1 ? '역삼성당' : '강남'}</strong> 입니다.
+                            </div>
                     </div>
-
-                    <div className="coupon">
-                        <div className="coupon__img"><img src="/static/img/survey/coupon-welcome.jpg" alt=""/></div>
-                        <div className="coupon__noti">
-                            <ul>
-                                <li>* 쿠폰은 라운지 체험 후 구매를 원하시면 자동 적용됩니다.</li>
-                                <li>* 결제할 때 별도의 쿠폰 이미지를 제시하실 필요는 없습니다.</li>
-                                <li>* 다른 할인 이벤트와 중복으로 사용 가능합니다.</li>
-                                <li>* 예약 취소 시 쿠폰은 자동 소멸됩니다.</li>
-                            </ul>
-                        </div>
-                    </div>
-
                 </div>
-
-            <div className="contentWrap">
-
-
-
+                <div className="coupon">
+                    <div className="coupon__img"><img src="/static/img/survey/coupon-welcome.jpg" alt="" /></div>
+                    <div className="coupon__noti">
+                        <ul>
+                            <li>* 쿠폰은 라운지 체험 후 구매를 원하시면 자동 적용됩니다.</li>
+                            <li>* 결제할 때 별도의 쿠폰 이미지를 제시하실 필요는 없습니다.</li>
+                            <li>* 다른 할인 이벤트와 중복으로 사용 가능합니다.</li>
+                            <li>* 예약 취소 시 쿠폰은 자동 소멸됩니다.</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </Layout>
     </>)
