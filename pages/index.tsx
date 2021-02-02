6b48fc30dc79fcdf007cacbef8e9a12d1d167749
yolkinthehouse <sbsbheo@gmail.com>
2021-02-02 10:15:18 +0900

    이벤트 배너 내용 수정 (삭제)기부이벤트 (추가)리뷰이벤트

--- pages/index.tsx
+++ pages/index.tsx
@@ -262,8 +262,8 @@ const IndexPage = (props: {
 
 								<div className="event-desc">
 									<span className="event-tag">이벤트</span>
-									<span className="event-text">안쓰는 안경 기부하면 안경 구매시</span>
-									<strong><span className="event-strong-m-only">안경기부, </span>최대 8만원 할인 혜택! &#xE001;</strong>
+									<span className="event-text">리뷰 남기면</span>
+									<strong><span className="event-strong-m-only">리뷰 남기면, </span>최대 3만원 할인 혜택! &#xE001;</strong>
 								</div>
 							</a>
 						</div>
