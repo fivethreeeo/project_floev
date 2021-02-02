3eaee44eaa1f8ee33e329e9b8bde25be3e19e137
yolkinthehouse <sbsbheo@gmail.com>
2021-02-02 15:31:22 +0900

    remove next/Link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import Head from 'next/head'
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { GetServerSideProps } from 'next'
 import Layout from '../layout/DefaultLayout'
