1f85bfe4000a9c6ad8ffcf088f4da4c6c0daac6f
yolkinthehouse <sbsbheo@gmail.com>
2021-02-10 09:29:35 +0900

    구글태그매니저 삭제

--- pages/_document.js
+++ pages/_document.js
@@ -33,16 +33,6 @@ class MyDocument extends Document {
         <Head>
           <script dangerouslySetInnerHTML={{ __html: this.renderSnippet() }} />
           <script async src="https://www.googletagmanager.com/gtag/js?id=AW-738487034"></script>
-          {/* 구글 태그매니저 */}
-          <script dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-NNF4ZC5');
-          `}}
-          />
           {/* 페이스북 픽셀 */}
           <script dangerouslySetInnerHTML={{
             __html: `
