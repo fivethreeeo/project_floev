4d47183b455af6bce96808781629029f48613796
InBaeJang <inbaejj117@gmail.com>
2021-02-24 14:50:41 +0900

    create fitting pages

--- pages/survey.tsx
+++ pages/survey.tsx
@@ -2,60 +2,7 @@ import React from 'react';
 import { GetServerSideProps } from 'next';
 import dynamic from 'next/dynamic'
 import { createApolloClient } from '../lib/apolloClient';
-import { UploadFile } from 'antd/lib/upload/interface'
 import { GET_PURCHASE_REQUEST_LIST } from '../lib/query'
-
-// 타입 정의
-declare global {
-    interface User {
-        id: string | undefined
-        name: string | undefined
-        email: string | undefined
-        password: string | undefined
-        phoneNumber: string | undefined
-        gender: string | undefined
-        birth: number | undefined
-        status: string | undefined
-        clickUpProfileId: string | undefined
-        requests: PurchaseRequest[]
-    }
-    interface FloevRequest {
-        [x: string]: any
-        id: string
-        date: string
-        loungeCode: number
-        type: number
-        status?: string
-    }
-    interface PurchaseRequest extends FloevRequest { }
-    interface PickupRequest extends FloevRequest { }
-
-    interface Slot {
-        time: string
-        loungeCode: number
-    }
-    // 설문, 예약 답변 양식
-    interface Answers {
-        customer: number // 0: 본인, 1: 동행, 2: 타인
-        birth: number
-        gender: string
-        hasWorn: number // 1: yes(쓰고 있음), 2: no(쓴적 없음)
-        purposes: string[]
-        purposeEtc: string
-        painDegree: number, painDegreeEtc: string
-        painTypes: string[], painTypesEtc: string
-        prefer: string
-        preferFileList: UploadFile[]
-        photoFileList: UploadFile[]
-        size: string
-        loungeCode: number
-        requestDate: string
-        requestTime: string
-        name: string
-        phoneNumber: string
-        authNumber: string
-    }
-}
 
 const DynamicComponent = dynamic(() => import('../components/survey/surveyComp'), {
     ssr: false,
