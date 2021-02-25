9c46ae823e4a4b79b0849492aa1b0eac3c16e6bc
sangbum.heo <sangbum.heo@floev.com>
2021-02-25 17:22:16 +0900

    install gtm

--- pages/_document.js
+++ pages/_document.js
@@ -50,6 +50,15 @@ class MyDocument extends Document {
           <noscript dangerouslySetInnerHTML={{
             __html: `<img height="1" width="1" style="display:none"
             src="https://www.facebook.com/tr?id=468879750384716&ev=PageView&noscript=1"/>` }} />
+            {/* Google Tag Manager */}
+            <script dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NNF4ZC5');
+            `}} />
         </Head>
         <body>
           <Main />
