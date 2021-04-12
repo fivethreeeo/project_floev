3aac4c9b458338cc9630f9e8eac3b8502e3b4fa6
InBaeJang <inbaejj117@gmail.com>
2021-04-12 16:41:02 +0900

    add cancel logic

--- components/emailModal.tsx
+++ components/emailModal.tsx
@@ -19,57 +19,52 @@ const EmailModal = (props: EmailModal) => {
         }
     }
 
+    function handleCancel() {
+        setEmail('')
+        setValidEmail(true)
+        setCompleted(false)
+        props.onCancel()
+    }
+
     async function handleSubmit() {
         if (email.length > 0 && validEmail) {
             await axios.post('', {
                 email: email,
                 testAgenda: props.testAgenda
             }).then(res => {
+                console.log(res.data)
                 setCompleted(true)
             }).catch(err => {
-
+                console.error("submit email error: " + err.message)
             })
 
         }
     }
 
+
     return (<>
-
-        {/* 이메일 신청 모달
-			
-				1. modal창이 처음 켜졌을때
-					- modalWrap 보임
-					- complete-m 안보임
-				
-				2. 이메일을 입력하고 신청하기 클릭 시
-					- modalWrap 안보임(사라짐)
-					-complete-m 보임(나타남)
-
-				3. modal창을 닫았다가 다시 열면 리셋
-
-			*/}
         <Modal
             className="mypage-modal-outer test8-modal"
             visible={props.visible}
-            onCancel={() => props.onCancel()}>
+            onCancel={() => handleCancel()}>
 
             <div className="mypage-modal">
-                <div className="email-form">
-                    <div className="email-form__input-box">
-                        <input className="email-form__input-box__input" value={email} type="text" placeholder="이메일 입력" id="test8-input-label--email" onChange={e => handleChange(e)} tabIndex={1} />
-                        {email.length > 0 ?
-                            (validEmail ? (<p>유효O</p>) : (<p>유효X</p>))
-                            : ('')}
+                {!completed ? (
+                    <div className="email-form">
+                        <div className="email-form__input-box">
+                            <input className="email-form__input-box__input" value={email} type="text" placeholder="이메일 입력" id="test8-input-label--email" onChange={e => handleChange(e)} tabIndex={1} />
+                            {email.length > 0 ?
+                                (validEmail ? (<p>유효O</p>) : (<p>유효X</p>))
+                                : ('')}
+                        </div>
+                        <div className="email-form__btn-box">
+                            <button className="email-form__btn-box__submit" type="submit" onClick={handleSubmit} tabIndex={2}>신청하기</button>
+                        </div>
                     </div>
-                    <div className="email-form__btn-box">
-                        <button className="email-form__btn-box__submit" type="submit" onClick={handleSubmit} tabIndex={2}>신청하기</button>
-                    </div>
-                </div>
-
-                {completed && (
+                ) : (
                     <div className="complete-m">
                         <div>이메일 신청이 완료됐습니다.</div>
-                        <div className="complete-m__btn-box"><button className="complete-m__btn-box__cloes" onClick={() => props.onCancel()}>확인</button></div>
+                        <div className="complete-m__btn-box"><button className="complete-m__btn-box__cloes" onClick={() => handleCancel()}>확인</button></div>
                     </div>
                 )}
             </div>
