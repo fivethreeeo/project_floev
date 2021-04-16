8005578a056c89c8202cdd26475454c21c6cd65a
IvyJ <inbaejj117@gmail.com>
2021-04-16 11:39:42 +0900

    test7 rollback

--- pages/FloevBuyNowPayLater.tsx
+++ pages/FloevBuyNowPayLater.tsx
@@ -1,2 +1,138 @@
-import redirect from 'nextjs-redirect'
-export default redirect('https://floev.com')
+import React, { useState } from 'react'
+import Layout from '../layout/DefaultLayout'
+import EmailModal from '../components/emailModal'
+import { NEW_SERVICE } from '../lib/constants'
+
+const FloevBuyNowPayLater = () => {
+  const [modal3, setModal3] = useState<boolean>(false)
+
+  function showModal(e: React.MouseEvent<HTMLButtonElement, MouseEvent>, modal: string) {
+    e.preventDefault(); // 修复 Android 上点击穿透
+    if (modal === 'modal3') {
+      setModal3(true)
+    }
+  }
+
+  return (
+    <Layout title="플로브 약정구매">
+      <div className="page__outer">
+        <div className="page__inner">
+          <div className="container">
+            {/* container__buynowpaylater */}
+            <div className="container__buynowpaylater">
+              {/* section__1 */}
+              <div className="section section__1">
+                <p className="p__1"><strong>플로브 약정구매 서비스</strong></p>
+                <p className="p__2">30만원 안경 <br /><strong>월 8,800원</strong>에<br />구매하세요.</p>
+              </div>
+
+              {/* section__2 */}
+              <div className="section section__2">
+                <p className="p__1"><strong>원하는 안경 오늘, 구매는 부담없이</strong></p>
+                <p className="p__2">약정선택 예시</p>
+                <div className="card">
+                  <div className="tag"><strong>A</strong></div>
+                  <div className="img__2"><img src="/img/test8/buynowpaylater/2.jpg" alt="" /></div>
+                  <p className="p__1">36만원 안경테 + 14만원 렌즈</p>
+                  <p className="p__2"><strong>= 총 50만원/24개월 선택</strong></p>
+                  <div className="img__arrow"><img src="/img/test8/buynowpaylater/arrow1.png" alt="" /></div>
+                  <p className="p__3"><strong>당일 15만원 결제</strong></p>
+                  <p className="p__4"><strong>이후 월 14,583원<span>/24개월</span></strong></p>
+                </div>
+                <div className="card b">
+                  <div className="tag"><strong>B</strong></div>
+                  <div className="img__3"><img src="/img/test8/buynowpaylater/3.jpg" alt="" /></div>
+                  <p className="p__1">22만원 안경테 + 8만원 렌즈</p>
+                  <p className="p__2"><strong>= 총 30만원/24개월 선택</strong></p>
+                  <div className="img__arrow"><img src="/img/test8/buynowpaylater/arrow2.png" alt="" /></div>
+                  <p className="p__3"><strong>당일 9만원 결제</strong></p>
+                  <p className="p__4"><strong>이후 <span className="color">월 8,800원</span><span>/24개월</span></strong></p>
+                </div>
+                <div className="card">
+                  <div className="tag"><strong>C</strong></div>
+                  <div className="img__4"><img src="/img/test8/buynowpaylater/4.jpg" alt="" /></div>
+                  <p className="p__1">32만원 안경테 + 40만원 렌즈</p>
+                  <p className="p__2"><strong>= 총 72만원/12개월 선택</strong></p>
+                  <div className="img__arrow"><img src="/img/test8/buynowpaylater/arrow1.png" alt="" /></div>
+                  <p className="p__3"><strong>당일 21.6만원 결제</strong></p>
+                  <p className="p__4"><strong>이후 월 42,000원<span>/12개월</span></strong></p>
+                </div>
+              </div>
+
+              {/* section__3 */}
+              <div className="section section__3">
+                <div className="div__1"><strong>플로브 약정구매 서비스</strong></div>
+                <div className="div__2">
+                  <div><strong>Buy Now</strong></div>
+                  <p className="p__1"><strong>30%만 지금,</strong></p>
+                  <p className="p__2">30만원짜리 안경 <strong>오늘은 9만원만</strong></p>
+                </div>
+                <div className="div__2">
+                  <div><strong>Pay Later</strong></div>
+                  <p className="p__1"><strong>나머지는 나누어 내세요</strong></p>
+                  <p className="p__2">12~24개월 <strong>원하는만큼 나눠서 결제</strong></p>
+                </div>
+              </div>
+
+              {/* section__4 */}
+              <div className="section section__4">
+                <div className="div__1">
+                  <p><strong>가장 합리적인 구매 방법<br />플로브 약정구매 서비스</strong></p>
+                </div>
+                <div className="div__2">
+                  <div className="num"><strong>1</strong></div>
+                  <p className="p__1"><strong>플로브 가격 그대로<br />할인 혜택도 그대로</strong></p>
+                  <p className="insert">장기 할부가 가능한 대신<br />원래 가격을 더 비싸게 받는 것 아닌가요?</p>
+                  <p className="p__2">아니요!<br />플로브 가격은 일반 소비자가에서 10-15%할인된 가격이고<br />진행중인 할인 이벤트의 혜택도 그대로 적용이 가능해요.</p>
+                </div>
+                <div className="div__2">
+                  <div className="num"><strong>2</strong></div>
+                  <p className="p__1"><strong>원하는대로<br />내 안경 금액만</strong></p>
+                  <p className="p__2">약정을 위한 최소 구매금액, 약정 가입비 등<br />약정구매를 위한 어떠한 구매 조건도 없습니다.<br />내가 원하는 안경/안경렌즈로 선택하세요.</p>
+                </div>
+                <div className="div__2 last">
+                  <div className="num"><strong>3</strong></div>
+                  <p className="p__1"><strong>할부 수수료 제로<br />늘어난 할부기간 선택</strong></p>
+                  <p className="p__2">플로브 약정구매는 카드사 제공 할부 기간보다 길고<br />할부 수수료는 없습니다.</p>
+                </div>
+              </div>
+
+              {/* section__5 */}
+              <div className="section section__5">
+                <div className="div__1"><strong>이런 분에게 추천하는 서비스</strong></div>
+                <div className="div__2">
+                  <p className="p__1"><strong>추천1</strong></p>
+                  <p className="p__2">안경은 마음에 드는데 가격 때문에 망설이는 분</p>
+                </div>
+                <div className="div__2">
+                  <p className="p__1"><strong>추천2</strong></p>
+                  <p className="p__2">시력이 나빠서 매번 고가의 렌즈를 구매하는 분</p>
+                </div>
+                <div className="div__2">
+                  <p className="p__1"><strong>추천3</strong></p>
+                  <p className="p__2">큰 지출이 부담스러워 매번 할부 결제를 하는 분</p>
+                </div>
+              </div>
+
+              {/* btn-more-wrap */}
+              <div className="btn-more-wrap">
+                <div className="btn-more">
+                  <button className="tn-0051" onClick={(e) => showModal(e, 'modal3')}>약정구매 더 알아보기</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <EmailModal
+        visible={modal3}
+        onCancel={() => setModal3(false)}
+        newService={NEW_SERVICE.BUY_NOW_PAY_LATER}
+      />
+    </Layout>
+
+  )
+}
+
+export default FloevBuyNowPayLater
--- pages/FloevTradeIn.tsx
+++ pages/FloevTradeIn.tsx
@@ -1,2 +1,143 @@
-import redirect from 'nextjs-redirect'
-export default redirect('https://floev.com')
+import React, { useState } from 'react'
+import Layout from '../layout/DefaultLayout'
+import EmailModal from '../components/emailModal'
+import { NEW_SERVICE } from '../lib/constants'
+
+const FloevTradeIn = () => {
+  const [modal3, setModal3] = useState<boolean>(false)
+
+  function showModal(e: React.MouseEvent<HTMLButtonElement, MouseEvent>, modal: string) {
+    e.preventDefault(); // 修复 Android 上点击穿透
+    if (modal === 'modal3') {
+      setModal3(true)
+    }
+  }
+
+  return (
+    <Layout title="플로브 반납보상">
+      <div className="page__outer">
+        <div className="page__inner">
+          <div className="container">
+            {/* container__tradein */}
+            <div className="container__tradein">
+              {/* section__1 */}
+              <div className="section section__1">
+                <div className="img__1"><img src="/img/test8/tradein/1.jpg" alt="" /></div>
+              </div>
+
+              {/* section__2 */}
+              <div className="section section__2">
+                <p className="p__1"><strong>플로브 반납보상</strong></p>
+                <p className="p__2"><strong>안경 반납하고 <br />현금 돌려받으세요.</strong></p>
+                <p className="p__3">플로브에서 구매한 안경을 반납하면 새 안경 구매 시 사용할 수 있는 크레딧 또는 현금으로 구매금액에 최대 35%를 돌려받을 수 있어요. 아니면 렌즈를 교환하거나, 주변 지인에게 크레딧을 양도할 수도 있어요. </p>
+              </div>
+
+              {/* section__3 */}
+              <div className="section section__3"><div className="img__2"><img src="/img/test8/tradein/2.jpg" alt="" /></div></div>
+
+              {/* section__4 */}
+              <div className="section section__4">
+                <p className="p__1"><strong>원하는 만큼 착용한 안경,<br />반납하고 구매금액의<br />35%까지 보상받아요.</strong></p>
+                <p className="p__2">플로브에서 구매한 안경을 반납하면 사용 기간과 테의 상태에 따라 크레딧 또는 현금으로 최대 35%까지 보상해드려요.</p>
+              </div>
+
+              {/* section__5 */}
+              <div className="section section__5">
+                <div className="div__1">
+                  <span className="span__1">사용기간</span>
+                  <span className="span__1">예상 크레딧</span>
+                  <span className="span__1">예상 현금</span>
+                </div>
+                <div className="div__2">
+                  <span className="span__2 lh-48">~ 6개월 미만</span>
+                  <span className="span__2 lh-48 big">최대 35%</span>
+                  <span className="span__2 lh-48 big">최대 30%</span>
+                </div>
+                <div className="line"></div>
+                <div className="div__2">
+                  <span className="span__2">6개월 이상<br />~ 12개월 미만</span>
+                  <span className="span__2 lh-48">최대 25%</span>
+                  <span className="span__2 lh-48">최대 20%</span>
+                </div>
+                <div className="line"></div>
+                <div className="div__2">
+                  <span className="span__2">12개월 이상<br />~ 18개월 미만</span>
+                  <span className="span__2 lh-48">최대 15%</span>
+                  <span className="span__2 lh-48">최대 10%</span>
+                </div>
+              </div>
+
+              {/* section__6 */}
+              <div className="section section__6">
+                <div className="div__1">구매할 때</div>
+                <div className="item">
+                  <div className="img__3"><img src="/img/test8/item__3.jpg" alt="" /></div>
+                  <p className="p__1"><strong>아이씨 베를린</strong></p>
+                  <p className="p__2">OROSHI BK</p>
+                  <div className="line"></div>
+                  <p className="p__3">68 <span className="won">만원</span></p>
+                </div>
+                <div className="img__4"><img src="img/test8/tradein/arrow.png" alt="" /></div>
+                <div className="div__2">6개월 후</div>
+                <div className="card-wrap">
+                  <div className="card__or">OR</div>
+                  <div className="card card__1">
+                    <span className="span__1">크레딧</span>
+                    <span className="span__4">만원</span>
+                    <span className="span__3">23.8</span>
+                    <span className="span__2">35%</span>
+                  </div>
+                  <div className="card card__2">
+                    <span className="span__1">현금</span>
+                    <span className="span__4">만원</span>
+                    <span className="span__3">20.4</span>
+                    <span className="span__2">30%</span>
+                  </div>
+                </div>
+                <div className="div__3">크레딧 또는 현금으로<br />보상을 선택해보세요!</div>
+              </div>
+
+              {/* section__7 */}
+              <div className="section section__7">
+                <div className="div__1">
+                  <p><strong>플로브 반납보상 서비스와<br />슬기로운 안경생활</strong></p>
+                </div>
+                <div className="div__2">
+                  <div className="num"><strong>1</strong></div>
+                  <p className="p__1"><strong>매번 여분 안경테가 늘어나<br />처치 곤란하신가요?</strong></p>
+                  <p className="p__2">일상생활은 1~2개의 여분 안경으로도 충분해요. 앞으로는 다양한 반납 보상 혜택으로 안경을 구매해보세요.</p>
+                </div>
+                <div className="div__2">
+                  <div className="num"><strong>2</strong></div>
+                  <p className="p__1"><strong>눈 건강을 위해<br />12~18개월의 안경 주기를 추천해요.</strong></p>
+                  <p className="p__2">안경 렌즈는 햇빛, 열, 스크레치의 원인으로  손상되요. 고급 렌즈보단 나에게 잘 맞는 렌즈를 자주 교체하는 게 좋아요. 플로브 반납 보상 서비스로 안경 교체의 부담을 줄여드릴게요.</p>
+                </div>
+                <div className="div__2 last">
+                  <div className="num"><strong>3</strong></div>
+                  <p className="p__1"><strong>부담없이 현금으로 돌려받으세요.</strong></p>
+                  <p className="p__2">고객에게 좋고, 플로브도 반납받은 안경테를 다양한 방식으로 활용해요. 플로브 자체 공정을 거치는 리퍼비쉬 제품 또는 플로브 크루 교육에 사용될 예정이에요.</p>
+                </div>
+              </div>
+
+              {/* btn-more-wrap */}
+              <div className="btn-more-wrap">
+                <div className="btn-more">
+                  <button className="tn-0058" onClick={(e) => showModal(e, 'modal3')}>반납보상 더 알아보기</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <EmailModal
+        visible={modal3}
+        onCancel={() => setModal3(false)}
+        newService={NEW_SERVICE.TRADE_IN}
+      />
+    </Layout>
+
+  )
+}
+
+export default FloevTradeIn
